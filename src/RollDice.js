313b019244f7c8fd210800ebb5954f8ab4369f7f
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2020-07-02 22:34:47 -0400

    Created comp

--- src/RollDice.js
+++ src/RollDice.js
@@ -4,19 +4,21 @@ import Die from './Die';
 const RollDice = () => {
   const sides = ['one', 'two', 'three', 'four', 'five', 'six'];
 
-  const [die, setDie] = useState({ die1: null, die2: null });
+  const [die, setDie] = useState({ die1: null, die2: null, rolling: false });
 
   const roll = () => {
     const newDie1 = sides[Math.floor(Math.random() * sides.length)];
     const newDie2 = sides[Math.floor(Math.random() * sides.length)];
-    setDie({ die1: newDie1, die2: newDie2 });
+    setDie({ die1: newDie1, die2: newDie2, rolling: true });
   };
 
   return (
     <div>
-      <Die face={die.die1} />
-      <Die face={die.die2} />
-      <button onClick={roll}>Roll It</button>
+      <div className='container'>
+        <Die face={die.die1} />
+        <Die face={die.die2} />
+        <button onClick={roll}>Roll It</button>
+      </div>
     </div>
   );
 };
