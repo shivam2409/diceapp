1971f64301c7ca600870b32325d290990337e721
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2020-07-03 21:56:17 -0400

    finalized

--- src/RollDice.js
+++ src/RollDice.js
@@ -4,21 +4,29 @@ import Die from './Die';
 const RollDice = () => {
   const sides = ['one', 'two', 'three', 'four', 'five', 'six'];
 
-  const [die, setDie] = useState({ die1: null, die2: null, rolling: false });
+  const [die, setDie] = useState({ die1: 'one', die2: 'two', rolling: false });
 
   const roll = () => {
     const newDie1 = sides[Math.floor(Math.random() * sides.length)];
     const newDie2 = sides[Math.floor(Math.random() * sides.length)];
     setDie({ die1: newDie1, die2: newDie2, rolling: true });
+
+    //wait one second and timeout
+
+    setTimeout(() => {
+      setDie({ ...die, rolling: false });
+    }, 1000);
   };
 
   return (
-    <div>
+    <div className='main'>
       <div className='container'>
         <Die face={die.die1} />
         <Die face={die.die2} />
-        <button onClick={roll}>Roll It</button>
       </div>
+      <button onClick={roll} disabled={die.rolling}>
+        {die.rolling ? 'Rolling..' : 'Roll Dice'}
+      </button>
     </div>
   );
 };
